e3ae3d573a400dbe46a74f63e9897f8a63ad3530
Elishaya13 <a.lartillier@gmail.com>
2024-06-12 09:43:30 +0200

    feat(storybook): add storybook for component development

---
+++ src/stories/Modal.stories.ts
@@ -0,0 +1,68 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Modal from '../components/Modal';
+
+const meta: Meta<typeof Modal> = {
+  title: 'Modal',
+  component: Modal,
+
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    isOpen: true,
+    styleIcon: 'check',
+    onClose: () => {},
+    title: 'Modal Title',
+    message: 'Modal message here !',
+    buttonMsg: 'Button',
+  },
+};
+
+export const Simple: Story = {
+  args: {
+    isOpen: true,
+    styleIcon: 'none',
+    onClose: () => {},
+    message: 'Simple modal !',
+    buttonMsg: 'Close',
+    btnBgColor: 'blue',
+    btnTextColor: 'white',
+    btnBorderRadius: '0px',
+    modalBorderRadius: '0px',
+  },
+};
+
+export const Custom: Story = {
+  args: {
+    isOpen: true,
+    styleIcon: 'check',
+    onClose: () => {},
+    title: 'Success!',
+    message: 'Element has been added successfully!',
+    buttonMsg: 'Close',
+    iconColor: 'green',
+    btnTextColor: 'white',
+    btnBgColor: '#5c469b',
+  },
+};
+
+export const ErrorModal: Story = {
+  args: {
+    isOpen: true,
+    styleIcon: 'error',
+    iconColor: 'red',
+    onClose: () => {},
+    title: 'Error',
+    message: 'An error has occurred !',
+    buttonMsg: 'OK',
+    btnBgColor: 'red',
+    btnTextColor: 'white',
+  },
+};
